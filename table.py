35729a368bb891bdbe4171dbf782b6bdd5c3a8cd
saranya <kowtharapusaranya08@gmail.com>
2025-09-01 22:53:01 +0530

    second commit

--- table.py
+++ table.py
@@ -1,1 +1,10 @@
-Print ('table')
+from google.cloud import bigquery
+client=bigquery.Client()
+table_id="my-project-saranya.saranya.customer_order"
+schema=[bigquery.SchemaField('cust_id','string',mode="required"),
+bigquery.SchemaField('cust_no','string',mode="required"),
+bigquery.SchemaField('cust_loc','string',mode="required"),
+bigquery.SchemaField('cust_email','string',mode="required")]
+table=bigquery.Table(table_id,schema=schema)
+table=client.create_table(table)
+print('table created successfully')
